6c5bd8f4f18df5a0ccf016a7aad7b13a22a9dd24
linghu86 <zhangfenghigh@gmail.com>
2015-10-13 17:56:29 +0800

    no message

---
+++ server/app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var port = 3000;
+var app = express();
+
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.set('views', __dirname + '/views/pages');
+app.set('view engine', 'jade');
+
+app.get('/', function(req,res){
+  res.render('index',{
+    title: '汽车商城 首页'
+  });
+});
+
+app.get('/car/:id', function(req,res){
+  res.render('car_detail',{
+    title: '汽车商城 详情页'
+  });
+});
+
+app.get('/admin/car/list', function(req,res){
+  res.render('car_list.jade',{
+    title: '汽车商城 列表页'
+  });
+});
+
+app.get('/admin/car/new', function(req,res){
+  res.render('car_admin',{
+    title: '汽车商城 后台录入页'
+  });
+});
+
+app.get('/admin/car/update/:id', function(req,res){
+  res.render('car_admin',{
+    title: '汽车商城 后台录入页'
+  });
+});
+
+app.listen(port);
+
+console.log("服务已启动,监听端口号:"+port+__dirname+"这个目录："+path);
